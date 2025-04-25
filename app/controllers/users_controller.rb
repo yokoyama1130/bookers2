79064ef6b7902cb57b97c8516325bb762b03f610
EC2 Default User <ryogotan@ezweb.ne.jp>
2025-04-25 10:25:45 +0000

    user_update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,21 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+
+  def update
+    @user = ユーザーの取得
+    @user.ユーザーのアップデート
+    redirect_to ユーザーの詳細ページへのパス  
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
+
+
+
+
 end
