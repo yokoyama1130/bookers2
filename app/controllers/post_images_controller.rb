e8416e38558e1607c6e878c2cc0da91c692bf56e
EC2 Default User <ryogotan@ezweb.ne.jp>
2025-04-28 00:10:41 +0000

    errar

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -25,6 +25,17 @@ class PostImagesController < ApplicationController
     redirect_to post_images_path
   end
 
+  # バリデーションエラーを出すかどうかのコントロール
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    if @post_image.save
+      redirect_to post_images_path
+    else
+      render :new
+    end
+  end
+
   private
 
   def post_image_params
